8d6a6f8bce9f7b7331e785d4e1650e2fdf3955ad
Rob Marvin <fenalfan@gmail.com>
2025-01-05 07:38:51 +0300

    feat: implement the function of removing an element by value from a single-linked list in remove-from-list.js

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (!l) return l;
+
+  let nextNode = l;
+  while (nextNode.next !== null) {
+    if (l.value === k) {
+      l = l.next;
+    }
+    if (nextNode.next.value === k) {
+      nextNode.next = nextNode.next.next;
+    } else {
+      nextNode = nextNode.next;
+    }
+  }
+
+  return l
 }
 
 module.exports = {
